b40fba698d2cb4408c2cb528d5492049a00bb0a4
Atif Afsar <atifafsar70@gmail.com>
2025-09-05 19:55:26 +0530

    Add image upload functionality and update dependencies

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 const postModel = require("../models/post.model");
 const { generateCaption } = require("../service/ai.service");
+const { uploadFile } = require("../service/srorage.service");
+const { v4: uuidv4 } = require('uuid');
 
 
 async function createPostController(req, res){
@@ -9,8 +11,14 @@ async function createPostController(req, res){
     const base64Image = Buffer.from(file.buffer).toString('base64');
 
     const caption = await generateCaption(base64Image);
+    const result = await uploadFile(file.buffer, `${uuidv4()}`);
     
-    res.json({caption});
+    const post = await postModel.create({
+        image: result.url,
+        caption,
+        user: req.user._id
+    });
+    return res.status(201).json({message:"Post created successfully", post})
 }
 
 module.exports = {createPostController}
